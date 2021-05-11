aed00286b9000639224ef7cab08f877cae0448cb
Kaycee <KayceeT2>
2021-05-11 11:09:02 -0400

    Initial Commit

---
+++ Reminders App/Controller/ViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Tableview Datasource Methods
+    
+    //MARK: - TableView Delegate Methods
+    
+    //MARK: - Add New Items
+    
+    //MARK: - Model Manipulation Methods
+}
+
+//MARK: - Search Bar Methods
